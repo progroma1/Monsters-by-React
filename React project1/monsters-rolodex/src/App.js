0171ec5d40e7a15ffff5918341f1ca26d5585c16
Roman <tyniv@progroma.com>
2021-04-09 11:09:31 +0300

    next version

--- React project1/monsters-rolodex/src/App.js
+++ React project1/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component'
 import './App.css';
+import {SearchBox} from './components/search-box/search-box.component';
 
 
 
@@ -8,7 +9,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField:''
     }
   }
 
@@ -18,15 +20,29 @@ componentDidMount() {
   .then(users => this.setState({monsters:users}));
 }
 
+handleChange = e => {
+  console.log(e.target);
+ // this.setState({searchField:e.target.value})
+}
+
   render() {
+    const {monsters,searchField} = this.state;
+    const filteredMonstered = monsters.filter( monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
 <div className="App">
-<CardList monsters={this.state.monsters} />
+      <SearchBox 
+      placeholder='search monster'
+      handleChange={this.handleChange}
+      />
+      <CardList monsters={filteredMonstered} />
   
-    </div>
+</div>
     )
   }
 }
+
 
 
 
